e7c33dc27380e48804ccc17b5d8e7c5690424106
yenzichun <yenping074@gmail.com>
2015-04-26 22:44:26 +0800

    first draft
    solve the major  part of the problem.
    make sure the functions are able to produce the right inverted matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,32 @@
 ## Put comments here that give an overall description of what your
-## functions do
+## functions do:
+
+## makeCacheMatrix(): to create a list object that stores a numeric vector.
+
+## cacheSolve(): to solve the inverted matrix that *** and cache's its inverted matrix.
+
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        ans <- NULL
+        set <- function(y) {
+            x <<- y 
+            ans <<- NULL
+        }
+        set_ans <- function(x) {
+            ans <<- x
+        }
+        get_ori_matrix <- function() {
+            x
+        }
+        get_ans <- function() {
+            ans
+        }
+        list(set = set, 
+             get = get_ori_matrix,
+             set_ans = set_ans,
+             get_ans = get_ans)
 }
 
 
@@ -12,4 +34,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inverted_m <- x$get_ans()
+        if(!is.null(inverted_m)) {
+            message("getting cached data")
+            return(inverted_m)
+        }
+        data <- x$get()
+        inverted_m <- solve(data, ...)
+        x$set_ans(inverted_m)
+        inverted_m
 }
